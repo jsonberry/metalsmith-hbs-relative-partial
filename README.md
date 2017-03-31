424b96a80627be8ea259791e99810ee394ba165f
citizenjasona <jason.awbrey@pluscitizen.com>
2017-03-31 10:10:06 -0700

    Add initial working version of the plugin

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # Metalsmith HBS Relative Partial Plugin
-Initial commit
+Install: npm install metalsmith-hbs-realtive-partial
+
+## Purpose
+
+Place Handlebars.js template partials in a directory relative to a view they are used in. Best used with in conjunction with [metalsmith-in-place](https://github.com/superwolff/metalsmith-in-place).
+
+## Example
+
+Given this structure...
+```
+/src
+|    /about
+|    |    /components
+|    |    |    heading.hbs
+|    index.hbs
+...
+```
+
+With this content in `heading.hbs`...
+```
+<h1>Hello World</h1>
+```
+
+And this Handlebars partial usage in `index.hbs`...
+```
+{{> about/heading }}
+```
+
+Metalsmith will produce `/about/index.html` with this content...
+```
+<h1>Hello World</h1>
+```
+
+## Initialize
+
+In `metalsmith.json`:
+
+```json
+{
+  "plugins": {
+    "metalsmith-hbs-relative-partial": {
+      "source": "src",
+      "pattern": "partials"
+    }
+  }
+}
+```
+
+```js
+const relativePartial = require('metalsmith-hbs-relative-partial');
+const inPlace = require('metalsmith-in-place');
+
+require('metalsmith')(__dirname)
+    .use(relativePartial({
+        source: 'src',
+        dirName: 'partials'
+    }))
+  .build()
+```
+
+### source
+The top level source directory of your app.
+Default: `src`
+
+### dirName
+The name of the directory that your partials will sit in.
+Example: 'components'... any partials nested under the source in a directory named 'components' will be registered with Handlebars.
+The name of the partial will be it's relative path to the area of the application. An `aside.hbs` partial in `src/legal/components` would be referred to like this: `{{> legal/aside }}`.
+Default: `partials`
---
+++ index.js
@@ -0,0 +1,82 @@
+'use strict'
+
+const { readFile: read } = require('fs');
+const { resolve, format, dirname, basename, extname } = require('path');
+const Handlebars = require('handlebars');
+
+
+module.exports = (opts) => {
+    const {
+        dirName: dirName = 'partials'
+    } = opts;
+
+    return function(files, metalsmith, done) {
+        const src = metalsmith.source();
+        const arrPromises = [];
+        const partials = getFiles(files, dirName);
+
+        if (!partials) {
+            console.log('Metalsmith HBS Relative Partial plugin exited: No partials found to process');
+            done();
+            return;
+        }
+
+        partials.forEach((file) => arrPromises.push(onFile(file, src, dirName)));
+
+        Promise.all(arrPromises)
+            .then((d) => d.forEach((p) => hbsRegPart(p)))
+            .then((d) => done())
+            .catch((e) => done(e));
+    };
+}
+
+/*
+*    Helpers
+*/
+
+function onFile(file, src, dirName) {
+    // if (!file) { throw new Error(`Metalsmith HBS Relative Partial Error - Invalid file: ${file}`) }
+    // if (!dirName) { throw new Error(`Metalsmith HBS Relative Partial Error - Invalid dirName: ${dirName}`) }
+
+    const dir = new RegExp(`/${dirName}`);
+    const directory = dirname(file).replace(dir, '');
+    const id = format({
+        directory: `${directory}`,
+        base: `${basename(file, extname(file))}`
+    });
+
+    return new Promise((res) => {
+        read(`${src}/${file}`, 'utf8', (err, contents) => {
+            res({
+                id: `${directory}/${id}`,
+                contents: contents,
+                file: file
+            });
+        });
+    })
+}
+
+function hbsRegPart(partial) {
+    const {
+        id: i = false,
+        contents: c = false,
+        file: file = false
+    } = partial;
+
+    if (!i) { throw new Error(`Metalsmith HBS Relative Partial Error - Partial needs a valid ID in order to process: ${file}`) }
+    if (!c) { throw new Error(`Metalsmith HBS Relative Partial Error - Partial needs valid contents in order to process: ${file}`) }
+
+    Handlebars.registerPartial(i, c)
+}
+
+function getFiles(files, dirName) {
+    let partials =  Object.keys(files).reduce((acc, val) => {
+        if (val.split('/').includes(dirName)) {
+            acc.push(val);
+        }
+
+        return acc;
+    }, []);
+
+    return (partials.length === 0 ?  false :  partials);
+}
